3b5f5736d8f36fd19d1f3ae9d8d1b8c137d88f5a
Sebbo92 <pohl.sebastian92@googlemail.com>
2025-10-05 21:01:51 +0200

    Fix installation of plugin

--- scripts/install.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-python -m venv $env:HELM_PLUGIN_DIR\.venv
-& $env:HELM_PLUGIN_DIR\.venv\Scripts\pip.exe install --upgrade pip
-& $env:HELM_PLUGIN_DIR\.venv\Scripts\pip.exe install -r $env:HELM_PLUGIN_DIR\requirements.txt
